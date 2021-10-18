4df6acef8de06d28245cc0e0939ca7ae536a44c8
Muhammad Abdullah Azzam <79858774+MuhammadAbdullahAzzam@users.noreply.github.com>
2021-10-18 21:43:59 +0700

    Add project files.

---
+++ Employee_018/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Employee_018.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
